fc5e37d28818e2e3d4be73d914cf37c9256612c0
Apurva Roshan <apurva7roshan@gmail.com>
2019-06-16 11:53:12 +0530

    16th june

---
+++ test0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 15 12:25:45 2019
+
+@author: AR
+"""
+
+import pandas as pd
+df=pd.read_csv(E:\pywork\pyprojects\sippython\RegularSeasonCompactResults.csv)
